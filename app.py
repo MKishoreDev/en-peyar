9b65e021eecaa53963261a4e9a5b025749148c96
Kishore M <kishoredxd@gmail.com>
2026-06-27 21:24:19 +0530

    Fix Google SEO: move JSON-LD inside head, add favicon.ico, fix absolute image URLs

--- app.py
+++ app.py
@@ -56,6 +56,10 @@ def home(): return render_template('index.html')
 @app.route('/about')
 def about(): return render_template('about.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_file('static/images/favicon.ico', mimetype='image/x-icon')
+
 @app.route('/manifest.json')
 def manifest():
     return send_file('static/manifest.json')
